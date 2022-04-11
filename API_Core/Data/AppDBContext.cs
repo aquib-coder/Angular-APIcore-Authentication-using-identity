75d5f90187c4af10f6b907ea969cff9daddfa206
Aquib Javed <atif9087@gmail.com>
2022-04-11 23:10:49 +0530

    First Commit-all packages installed

---
+++ API_Core/Data/AppDBContext.cs
@@ -0,0 +1,18 @@
+﻿using API_Core.Data.Entities;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API_Core.Data
+{
+    public class AppDBContext : IdentityDbContext<AppUser,IdentityRole,string>
+    {
+        public AppDBContext(DbContextOptions options) : base(options)
+        {
+        }
+    }
+}
---
+++ API_Core/Data/Entities/AppUser.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API_Core.Data.Entities
+{
+    public class AppUser : IdentityUser
+    {
+        public string FUllName { get; set; }
+        public DateTime DateCreated { get; set; }
+        public DateTime DateModified{ get; set; }
+       
+    }
+}
